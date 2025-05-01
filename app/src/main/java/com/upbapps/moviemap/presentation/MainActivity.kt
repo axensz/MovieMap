fcdf15de569162d8d36104ab0903268f412adee6
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-04-30 21:58:14 -0500

    Definición de navegación y creación de vista login

--- app/src/main/java/com/upbapps/moviemap/presentation/MainActivity.kt
+++ app/src/main/java/com/upbapps/moviemap/presentation/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
 import com.upbapps.moviemap.ui.theme.MovieMapTheme
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.upbapps.moviemap.presentation.vistas.Login
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieMapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "login"){
+                    composable("login") { Login(navController) }
                 }
             }
         }
